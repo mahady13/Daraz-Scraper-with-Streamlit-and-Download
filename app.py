305e4218f879951dabdcc26979acf45527549c8f
mahady13 <mohiuddinmahady@gmail.com>
2026-03-28 03:10:28 +0600

    Fixed deployment issue 3rd time

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/google-chrome"
 import time
 
 data={'Title':[],'Price':[],'Location':[],'Link':[],'ImageLink':[]}
